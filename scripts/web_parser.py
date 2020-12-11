305eb6bcca06ed6866bed6121c12a101c9ec6d45
MillanUka <ukamillan@gmail.com>
2020-12-11 17:34:57 +1300

    Script now convert the webpage to a json file

--- scripts/web_parser.py
+++ scripts/web_parser.py
@@ -1,7 +1,30 @@
+from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
+import re
 import json
-f = open("./forex.html", "r")
-jsonData = json.dumps(f.read())
-f = open('data.json', 'w') 
-f.write(jsonData)
-f.close() 
 
+f = open("forex.html")
+soup = BeautifulSoup(f.read(), "html.parser")
+
+table = soup.find(lambda tag: tag.name=='table' and tag.has_attr('style') and tag['style']=="border-collapse:separate;border-spacing:1px;max-width:1264px;margin:0 auto") 
+rawData = table.findAll("td", {"class": "body-table"})
+rows = []
+for i in rawData:
+      i = i.contents[0]
+      cleanr = re.compile('<.*?>')
+      i = re.sub(cleanr, '', (str)(i))
+      rows.append(i)
+
+reshaped = []
+json_data = []
+for i in range(0, 10):
+    row = rows[(i*12):(i*12)+12]
+    reshaped.append(row)
+    json_data.append({'No.' : (int)(row[0]), 'Ticker': row[1], 'Price' : row[2], 'Perf 5Min' : row[3], 'Perf Hour' : row[4], 'Perf Day' : row[5], 'Perf Week' : row[6], 'Perf Month' : row[7], 'Perf Quart' : row[8], 'Perf Half' : row[9], 'Perf Year' : row[10], 'Perf YTD' : row[11]})
+
+f = open("data.json", "w")
+f.write(json.dumps(json_data))
+f.close()
+
+print("Done")
